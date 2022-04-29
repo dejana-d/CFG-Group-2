4483cc280b6e56273cefd51cf103d60bc4e91d3a
dejana-d <103682312+dejana-d@users.noreply.github.com>
2022-04-29 16:35:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,470 @@
-// javascript file 
+// CODE ADAPTED FROM:
+// https://jsfiddle.net/h0yu5sL2/ 
+// https://www.w3schools.com/jsref/met_element_addeventlistener.asp 
+
+
+// FUNCTION TO OPEN LINK IN NEW TAB WHEN CLICKED //
+function openInNewTab(url) {
+  const win = window.open(url, '_blank');
+//   win.focus();
+}
+
+// FUNCTION TO ALERT WHEN WRONG DAY IS CLICKED//
+function wrongDay(){
+    alert("come back on the right day :)");
+}
+
+
+//DATES AND LINKS SET FOR EACH DAY BELOW //
+
+// MAY 1
+const day1 = document.getElementsByClassName("item1");
+const todayIs1 = new Date();
+let getDay1 = todayIs1.getDate()
+
+function may1(){
+if ((getDay1 >= 1)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 2
+const day2 = document.getElementsByClassName("item2");
+const todayIs2 = new Date();
+let getDay2 = todayIs2.getDate()
+
+function may2(){
+if ((getDay2 >= 2)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 3
+const day3 = document.getElementsByClassName("item3");
+const todayIs3 = new Date();
+let getDay3 = todayIs3.getDate()
+
+function may3(){
+if ((getDay3 >= 3)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 4
+const day4 = document.getElementsByClassName("item4");
+const todayIs4 = new Date();
+let getDay4 = todayIs4.getDate()
+
+function may4(){
+if ((getDay4 >= 4)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 5
+const day5 = document.getElementsByClassName("item5");
+const todayIs5 = new Date();
+let getDay5 = todayIs5.getDate()
+
+function may5(){
+if ((getDay5 >= 5)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 6
+const day6 = document.getElementsByClassName("item6");
+const todayIs6 = new Date();
+let getDay6 = todayIs6.getDate()
+
+function may6(){
+if ((getDay6 >= 6)  ){
+  openInNewTab('https://open.spotify.com/playlist/37i9dQZF1DX1g0iEXLFycr?si=44ff55d316c54ede');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 7
+const day7 = document.getElementsByClassName("item7");
+const todayIs7 = new Date();
+let getDay7 = todayIs7.getDate()
+
+function may7(){
+if ((getDay7 >= 7)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 8
+const day8 = document.getElementsByClassName("item8");
+const todayIs8 = new Date();
+let getDay8 = todayIs8.getDate()
+
+function may8(){
+if ((getDay8 >= 8)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 9
+const day9 = document.getElementsByClassName("item9");
+const todayIs9 = new Date();
+let getDay9 = todayIs9.getDate()
+
+function may9(){
+if ((getDay9 >= 9)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 10
+const day10 = document.getElementsByClassName("item10");
+const todayIs10 = new Date();
+let getDay10 = todayIs10.getDate()
+
+function may10(){
+if ((getDay10 >= 10)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 11
+const day11 = document.getElementsByClassName("item11");
+const todayIs11 = new Date();
+let getDay11 = todayIs11.getDate()
+
+function may11(){
+if ((getDay11 >= 11)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 12
+const day12 = document.getElementsByClassName("item12");
+const todayIs12 = new Date();
+let getDay12 = todayIs12.getDate()
+
+function may12(){
+if ((getDay12 >= 12)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 13
+const day13 = document.getElementsByClassName("item13");
+const todayIs13 = new Date();
+let getDay13 = todayIs13.getDate()
+
+function may13(){
+if ((getDay13 >= 13)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 14
+const day14 = document.getElementsByClassName("item14");
+const todayIs14 = new Date();
+let getDay14 = todayIs13.getDate()
+
+function may14(){
+if ((getDay14 >= 14)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 15
+const day15 = document.getElementsByClassName("item15");
+const todayIs15 = new Date();
+let getDay15 = todayIs15.getDate()
+
+function may15(){
+if ((getDay15 >= 15)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 16
+const day16 = document.getElementsByClassName("item16");
+const todayIs16 = new Date();
+let getDay16 = todayIs16.getDate()
+
+function may16(){
+if ((getDay16 >= 16)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 17
+const day17 = document.getElementsByClassName("item17");
+const todayIs17 = new Date();
+let getDay17 = todayIs17.getDate()
+
+function may17(){
+if ((getDay17 >= 17)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 18
+const day18 = document.getElementsByClassName("item18");
+const todayIs18 = new Date();
+let getDay18 = todayIs18.getDate()
+
+function may18(){
+if ((getDay18 >= 18)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 19
+const day19 = document.getElementsByClassName("item19");
+const todayIs19 = new Date();
+let getDay19 = todayIs19.getDate()
+
+function may19(){
+if ((getDay19 >= 19)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 20
+const day20 = document.getElementsByClassName("item20");
+const todayIs20 = new Date();
+let getDay20 = todayIs20.getDate()
+
+function may20(){
+if ((getDay20 >= 20)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 21
+const day21 = document.getElementsByClassName("item21");
+const todayIs21 = new Date();
+let getDay21 = todayIs21.getDate()
+
+function may21(){
+if ((getDay21 >= 21)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 22
+const day22 = document.getElementsByClassName("item22");
+const todayIs22 = new Date();
+let getDay22 = todayIs22.getDate()
+
+function may22(){
+if ((getDay22 >= 22)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 23
+const day23 = document.getElementsByClassName("item23");
+const todayIs23 = new Date();
+let getDay23 = todayIs21.getDate()
+
+function may23(){
+if ((getDay23 >= 23)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 24
+const day24 = document.getElementsByClassName("item24");
+const todayIs24 = new Date();
+let getDay24 = todayIs24.getDate()
+
+function may24(){
+if ((getDay24 >= 24)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 25
+const day25 = document.getElementsByClassName("item25");
+const todayIs25 = new Date();
+let getDay25 = todayIs25.getDate()
+
+function may25(){
+if ((getDay25 >= 25)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 26
+const day26 = document.getElementsByClassName("item26");
+const todayIs26 = new Date();
+let getDay26 = todayIs26.getDate()
+
+function may26(){
+if ((getDay26 >= 26)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 27
+const day27 = document.getElementsByClassName("item27");
+const todayIs27 = new Date();
+let getDay27 = todayIs27.getDate()
+
+function may27(){
+if ((getDay27 >= 27)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 28
+const day28 = document.getElementsByClassName("item28");
+const todayIs28 = new Date();
+let getDay28 = todayIs28.getDate()
+
+function may28(){
+if ((getDay28 >= 28)  ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+
+// MAY 29
+const day29 = document.getElementsByClassName("item29");
+const todayIs29 = new Date();
+let getDay29 = todayIs29.getDate()
+
+function may29(){
+if ((getDay29 >= 29 ) ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 30
+const day30 = document.getElementsByClassName("item30");
+const todayIs30 = new Date();
+let getDay30 = todayIs30.getDate()
+
+function may30(){
+if ((getDay30 >= 30 ) ){
+  openInNewTab('http://www.google.com');
+  } else {
+      wrongDay();
+  }
+}
+
+
+// MAY 31
+const day31 = document.getElementsByClassName("item31");
+const todayIs31 = new Date('31');
+let getDay31 = todayIs31.getDate()
+
+function may31(){
+if ((getDay31 >= 31 ) ){
+  openInNewTab('page31.html');
+  } else {
+      wrongDay();
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // to toggle text on button from reading 'light' and 'dark'. 
@@ -15,6 +481,9 @@ function toggleText() {
     }
   }
 
+
+
+
 // function for dark mode, see toggle class in index.html where it is called
 // code adapted from: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_toggle_dark_mode
 
@@ -25,13 +494,6 @@ function darkMode() {
   //  calling function, removed from html
   toggleText();
 }
-
-
-
-
-
-
-
 
 
 
